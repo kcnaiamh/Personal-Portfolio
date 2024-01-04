f0ae5ab0cc90079a59f43d48e4ef6cde1260422b
Naim <naimul9m@gmail.com>
2024-01-04 12:16:08 +0600

    ekhon ami portfolio compoenent e tab banate jacchi..most probbably break korbe

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "../layout/Layout";
 import getCerts from "../APIRequest/APIRequest";
-import { RxCross2 } from "react-icons/rx";
 
 const PortfolioPage = () => {
 	const [certs, setCerts] = useState([]);
 
-	const [imageSerial, setImageSerial] = useState(0);
-	const [openModal, setShowModal] = useState(false);
+	const [cardIndex, setCardIndex] = useState(0);
+	const [showModal, setShowModal] = useState(false);
 
 	const modalRef = useRef();
 
@@ -19,26 +18,25 @@ const PortfolioPage = () => {
 	}, []);
 
 	const handleOpenModal = (index) => {
-		setImageSerial(index);
+		setCardIndex(index);
 		setShowModal(true);
 	};
 
 	return (
 		<Layout>
-			{openModal && (
+			{showModal && (
 				<div
 					ref={modalRef}
 					onClick={(e) => {
 						modalRef.current === e.target && setShowModal(false);
 					}}
-					className="fixed z-10 inset-0 bg-black bg-opacity-30 backdrop-blur-md flex justify-center items-center">
+					className="fixed z-10 inset-0 bg-black bg-opacity-80 backdrop-blur-md flex justify-center items-center">
 					<div className="max-w-md">
-						{/* <button
-							onClick={() => setShowModal(false)}
-							className="place-self-end align-start">
-							<RxCross2 />
-						</button> */}
-						<img src={certs[imageSerial].image_path} alt="" />
+						<img src={certs[cardIndex].image_path} alt="" />
+						<div>
+							<p>{certs[cardIndex].name}</p>
+							<p>{certs[cardIndex].description}</p>
+						</div>
 					</div>
 				</div>
 			)}
@@ -48,9 +46,14 @@ const PortfolioPage = () => {
 					certs.map((item, index) => {
 						return (
 							<figure
-								className="flex-1 m-3 hover:scale-105 duration-200"
+								className="group flex-1 m-3 relative hover:scale-105 duration-200"
 								onClick={() => handleOpenModal(index)}>
 								<img src={item["image_path"]} alt="" />
+
+								<div className=" absolute bottom-0 left-0 right-0 bg-black bg-opacity-75 overflow-hidden w-full  transition-all duration-500 ease-in-out h-0 group-hover:h-1/2">
+									<p>{item["name"]}</p>
+									<p>{item["description"]}</p>
+								</div>
 							</figure>
 						);
 					})}

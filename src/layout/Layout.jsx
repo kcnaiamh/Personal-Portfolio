dd2f131241ae9df0507538794d9b8c79147f7549
Naim <naimul9m@gmail.com>
2024-01-06 22:23:29 +0600

    all good

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,15 +5,17 @@ import { Toaster } from "react-hot-toast";
 
 const Layout = (props) => {
 	return (
-		<div className="flex-col pt-20 min-h-screen bg-black">
-			<Header />
-			<div className="flex space-x-11 mx-56 px-56">
-				<SideBar />
-				<div className=" text-white bg-[#14213d] rounded-lg shadow-lg shadow-[#14213D] w-full px-6 py-6 mb-24">
-					{props.children}
+		<div className="bg-black relative min-h-screen">
+			<div className="flex-col pt-20 bg-black absolute max-w-screen">
+				<Header />
+				<div className="flex space-x-11 mx-56 px-56">
+					<SideBar />
+					<div className=" text-white bg-[#14213d] rounded-lg shadow-lg shadow-[#14213D] w-full px-6 py-6 mb-24">
+						{props.children}
+					</div>
 				</div>
+				<Toaster position="bottom-right" reverseOrder={false} />
 			</div>
-			<Toaster position="bottom-right" reverseOrder={false} />
 		</div>
 	);
 };
